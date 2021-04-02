24146e40b84ccd411d2913474d1fe5efe9d6fd3b
aaronh1986 <aaronharbourne@gmail.com>
2021-04-02 23:06:33 +0000

    Add some more messing around

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ const websiteTool = "https://www.toolband.com/news"
 document.getElementById('tool').innerHTML = `
                                              <h1>TOOL are a decent band</h1>
                                              <a target="_blank" href=${websiteTool}>The official TOOL band page</a>`;
+
+
+ let h5 = document.querySelector('h5');
+ 
+h5.addEventListener('mouseover', () => {
+    h5.style.color = "green";
+})
+h5.addEventListener('mouseout', () => {
+    h5.style.color = "black";
+}) 
+
+
+h5.addEventListener('click', function() {
+    h5.style.fontSize = "4rem"
+})
+
+
+   
+
