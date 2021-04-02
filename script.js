717557c3b0e8c08c99706875312cea258a367fbf
aaronh1986 <aaronharbourne@gmail.com>
2021-04-02 22:45:26 +0000

    Add javascript attempts

---
+++ script.js
@@ -0,0 +1,7 @@
+const website = document.getElementById('web').setAttribute('href', "https://www.toolband.com/news");
+
+const websiteTool = "https://www.toolband.com/news"
+
+document.getElementById('tool').innerHTML = `
+                                             <h1>TOOL are a decent band</h1>
+                                             <a target="_blank" href=${websiteTool}>The official TOOL band page</a>`;
